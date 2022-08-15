1c9e2af8991bedbd13449945b0116936d320295f
hyunsooryu <41038031+hyunsooryu@users.noreply.github.com>
2022-08-15 19:47:40 +0900

    create sample project

---
+++ src/main/java/com/hyunsoo/springbootwas/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.hyunsoo.springbootwas.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String getHello(){
+        return "Hello";
+    }
+
+}
